905c7668f4ecb16736de136b5eae5edc8af1e39e
WojtasBjj <wojtekgajda94@gmail.com>
2021-06-01 20:13:38 +0200

    added show all gyms function

--- src/components/FindGym.vue
+++ src/components/FindGym.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="co">
-        <ul class="gyms"></ul>
-        <button>Load All Gyms</button>
+        <ul id="gyms" class="gyms">
+            <li></li>
+        </ul>
+        <button @click="getData">Load All Gyms</button>
     </div>
 </template>
 
@@ -9,11 +11,42 @@
 export default {
     data() {
         return {
-
+            gyms: [],
         }
     },
     methods: {
+        getData() {
+            fetch('https://find-your-gym-default-rtdb.firebaseio.com/gyms.json').then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+            }).then((data) => {
+                const results = []
+                for (let i in data) {
+                    results.push({
+                        gymName: data[i].gymName,
+                        city: data[i].city,
+                        gymStreet: data[i].gymStreet,
+                        mainCoach: data[i].mainCoach,
+                        description: data[i].description
+                    })
+                }
+                this.gyms = results
+                const gymList = document.getElementById('gyms')
+                console.log(gymList)
 
+                for(let i = 0; i < this.gyms.length; i++) {
+                    const newLi = document.createElement('li')
+                    newLi.innerHTML = `<h1>Nazwa: ${this.gyms[i].gymName}<h1><h2>Miasto: ${this.gyms[i].city}, ${this.gyms[i].gymStreet}</h2><h2>Miasto: ${this.gyms[i].mainCoach}</h2><p>${this.gyms[i].description}</p>`
+                    // newLi.innerHTML = this.gyms[i].gymName
+                    console.log(newLi)
+                    gymList.appendChild(newLi)
+                }
+                
+
+
+            })
+        }
     }
 }
 </script>
